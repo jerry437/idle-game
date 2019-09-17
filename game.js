bc875681af45b2fa99b04c8cfe3779838c60867a
gerald <gxbaha60@harrisonburg.k12.va.us>
2019-09-17 13:33:23 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,13 +2,20 @@ var score = 0;
 
 postToPage(score); // Print to the page
 
-createButton('Button', btnPress); // Make a button
+createButton('gerald', btnPress); // Make a button
 changeTitle('Clicker Game');
 
 setInterval(btnPress, 1000);
 
 function btnPress() {
-  score++;
-  postToPage(score);
+  score--;
+  postToPage(score)
 }
-
+function gerald() {
+  score++;
+  postToPage(score)
+}
+createButton('Button', btnPress); // Make a button
+changeTitle('Clicker Game');
+score++;
+postToPage(score)
