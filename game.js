55f182206b924a026503237cde818507af3752b8
gerald <gxbaha60@harrisonburg.k12.va.us>
2019-09-27 14:13:13 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,30 +1,19 @@
-var score = 23; // Create a variable to store the score in
-
+var score = 15; // Create a variable to store the score in
+var settraps = 10;
 var title = new Title('welcome to the jungle'); // Create a title element
-var button = new Button('collect wood', btnPress);   // Create a button element
-var hidden = new Button('set traps');
+var collect = new Button1('collect wood', btnPress);   // Create a button element
+var settraps = new Button2('set traps');
+var  lightfire = new Button3('light fire');
+
 hidden.hide();
 var scr = new Text(score);                    // Create a text element
-
-
-changeTitle('Clicker Game');
+var scr = new text(score);
 
 function btnPress() {
   score++;         // Increase the score
   scr.edit(score); // Update the page with the new score
   if(score > 10) {
     hidden.show();
+
   }
-  if(score > 15) {
-    hidden.hide(500);
-  if(score > 25) {
-    hidden.hide(500);
-  }
-  if(score > 20) {
-    hidden.show(500);
-  }
-}
-createButton('Button', btnPress); // Make a button
-changeTitle('Clicker Game');
-score++;
-postToPage(score)
+}  
