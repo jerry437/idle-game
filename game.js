8c7e959e0e8ef7bd07cbd3cb62eb8de045a70eb2
gerald <gxbaha60@harrisonburg.k12.va.us>
2019-11-19 13:21:01 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,19 +1,23 @@
-var score = 15; // Create a variable to store the score in
-var settraps = 10;
-var title = new Title('welcome to the jungle'); // Create a title element
-var collect = new Button1('collect wood', btnPress);   // Create a button element
-var settraps = new Button2('set traps');
-var  lightfire = new Button3('light fire');
+var score = 1; // Create a variable to store the score in
 
+var title = new Title('Lost in the Forest'); // Create a title element
+var button = new Button('collect wood', btnPress);   // Create a button element
+var hidden = new Button('set traps',btnPress);
 hidden.hide();
 var scr = new Text(score);                    // Create a text element
-var scr = new text(score);
+
+
 
 function btnPress() {
   score++;         // Increase the score
   scr.edit(score); // Update the page with the new score
   if(score > 10) {
     hidden.show();
-
   }
-}  
+  if(score > 15) {
+    hidden.hide(500);
+  }
+  if(score > 20) {
+    hidden.show(500);
+  }
+}
